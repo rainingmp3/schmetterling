Fixed rotation matrix + added some readablity

rotation_matrix.py
@@ -3,17 +3,17 @@ import numpy as np
 class RotationMatrix:
     def __init__(self, parent):
         self.parent = parent
-    
+        
     def get_rotation_matrix(self):
         phi, theta, psi = self.parent.eule
-        
-        R = np.array([
-            [np.cos(theta) * np.cos(psi), -np.cos(theta) * np.sin(psi), np.sin(theta)],
-            [np.cos(phi) * np.sin(psi) + np.sin(phi) * np.sin(theta) * np.cos(psi),
-             np.cos(phi) * np.cos(psi) - np.sin(phi) * np.sin(theta) * np.sin(psi),
-             -np.sin(phi) * np.cos(theta)],
-            [np.sin(phi) * np.sin(psi) - np.cos(phi) * np.sin(theta) * np.cos(psi),
-             np.sin(phi) * np.cos(psi) + np.cos(phi) * np.sin(theta) * np.sin(psi),
-             np.cos(phi) * np.cos(theta)]
-        ])
+        C_phi = np.cos(phi)
+        C_theta = np.cos(theta)
+        C_psi = np.cos(psi)
+        S_phi = np.sin(phi)
+        S_theta = np.sin(theta)
+        S_psi =  np.sin(psi)
+        R = np.array([[C_psi*C_theta, C_psi*S_theta*S_phi - S_psi*C_phi, C_psi*S_theta*C_phi + S_psi*S_phi],
+                      [S_psi*C_theta, S_psi*S_theta*S_phi - C_psi*C_phi, S_psi*S_theta*C_phi + C_psi*S_phi],
+                      [     -S_theta,                    C_theta*S_phi ,                     C_theta*C_phi],        
+                      ])
         return R